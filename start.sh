d3862cb9be26bf958f73209ac563818717516de8
cheney.yan <cheney.yan@gmail.com>
2019-07-09 11:23:06 +1000

    Fix start script

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat /credential.yaml| grep password: | awk -F':' '{print $NF}'|xargs | /openconnect/openconnect --protocol=gp --protocol=$(cat credential.yaml| grep protocol: | awk -F':' '{print $NF}'|xargs) $(cat credential.yaml| grep host: | awk -F':' '{print $NF}'|xargs) -u $(cat credential.yaml| grep username: | awk -F':' '{print $NF}'|xargs) --passwd-on-stdin
+cat /credential.yaml| grep password: | awk -F':' '{print $NF}'|xargs | /openconnect/openconnect --protocol=gp --protocol=$(cat /credential.yaml| grep protocol: | awk -F':' '{print $NF}'|xargs) $(cat /credential.yaml| grep host: | awk -F':' '{print $NF}'|xargs) -u $(cat /credential.yaml| grep username: | awk -F':' '{print $NF}'|xargs) --passwd-on-stdin
